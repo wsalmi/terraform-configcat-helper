3d70c612d3c24c0bba1247090ea63e1b820a2f5f
Wesley Salmi <wsalmi@latam.stefanini.com>
2021-09-20 15:05:55 -0300

    Add output description

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "flags" {
   value = configcat_setting.main
+  description = "All flags registered inside module"
 }
 
 output "flags_values" {
   value = configcat_setting_value.main
+  description = "All flags values"
 }
 
 output "tags" {
   value = configcat_tag.main
+  description = "All tags registered inside module"
 }
